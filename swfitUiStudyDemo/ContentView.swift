700df77a43d29d11c5b3929aa998842bf1bbd992
gcl-ant <36355840+gclsgithub@users.noreply.github.com>
2026-04-06 14:47:38 +0900

    init

---
+++ swfitUiStudyDemo/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  swfitUiStudyDemo
+//
+//  Created by 顧燦林 on 2025/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+//            Color.blue.ignoresSafeArea()
+            
+            VStack {
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                    Text("AABBCCDD")
+                    Spacer()
+                }
+            }.padding(.top, 44 )
+            
+        }
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swfitUiStudyDemo/swfitUiStudyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swfitUiStudyDemoApp.swift
+//  swfitUiStudyDemo
+//
+//  Created by 顧燦林 on 2025/06/09.
+//
+
+import SwiftUI
+
+@main
+struct swfitUiStudyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
